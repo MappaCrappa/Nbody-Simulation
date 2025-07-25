2b89c1fbe88c0bc55d0add4a9d8b2af335f96df2
MappaCrappa <mappapetts@gmail.com>
2025-07-25 15:16:49 +0200

    Added time-tracking Combined force functions for ease of calling Converted from numpy to numba for performance Made visualisation a function for clarity and ease of use. Added time elapsed.

--- functions.py
+++ functions.py
@@ -1,5 +1,11 @@
 import numpy as np
+from numba import njit, prange
 from scipy.fft import fftn, ifftn
+from simulation_config import *
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # Needed for 3D plotting
+import imageio as iio
+import time
 
 # Nearest Grid Point assignment
 def NGP(positions, grid_size, box_size, masses):
@@ -10,28 +16,27 @@ def NGP(positions, grid_size, box_size, masses):
     return density
 
 # Cloud-in-Cell assignment
+@njit(parallel=True, fastmath=True)
 def CIC(positions, grid_size, box_size, masses):
-    density = np.zeros((grid_size,)*3, dtype=float)
-    positions = positions % box_size
+    density = np.zeros((grid_size, grid_size, grid_size), dtype=float)
     cell_size = box_size / grid_size
-    # Find normalized cell indices and weights
-    scaled_pos = positions / cell_size
-    i = np.floor(scaled_pos).astype(int)
-    d = scaled_pos - i
-    for p in range(positions.shape[0]):
-        base = i[p]
-        w = d[p]
-        for dx in [0, 1]:
-            wx = (1 - w[0]) if dx == 0 else w[0]
-            x = (base[0] + dx) % grid_size
-            for dy in [0, 1]:
-                wy = (1 - w[1]) if dy == 0 else w[1]
-                y = (base[1] + dy) % grid_size
-                for dz in [0, 1]:
-                    wz = (1 - w[2]) if dz == 0 else w[2]
-                    z = (base[2] + dz) % grid_size
-                    weight = wx * wy * wz
-                    density[x, y, z] += masses[p] * weight
+    for p in prange(positions.shape[0]):    #For Nth particle in Positions
+        position = positions[p] % box_size
+        mass = masses[p]
+        # Find normalized cell indices and weights
+        scaled_pos = position / cell_size
+        i = np.floor(scaled_pos).astype(np.int64)
+        d = scaled_pos - i
+        for dx in (0, 1):
+            wx = 1.0 - d[0] if dx == 0 else d[0]
+            x = (i[0] + dx) % grid_size
+            for dy in (0, 1):
+                wy = 1.0 - d[1] if dy == 0 else d[1]
+                y = (i[1] + dy) % grid_size
+                for dz in (0, 1):
+                    wz = 1.0 - d[2] if dz == 0 else d[2]
+                    z = (i[2] + dz) % grid_size
+                    density[x, y, z] += mass * wx * wy * wz
     return density
 
 # Potential
@@ -40,7 +45,6 @@ def compute_potential(density, grid_size):
     kx, ky, kz = np.meshgrid(k, k, k, indexing='ij')
     k2 = kx**2 + ky**2 + kz**2
     k2[0, 0, 0] = 1     #Division by 0 clause
-    #density_k = fftn(density)
     density_mean = np.mean(density)
     density_k = fftn(density - density_mean)
     potential_k = density_k / k2
@@ -49,6 +53,44 @@ def compute_potential(density, grid_size):
     return potential
 
 #Force
+def force(potential, positions, grid_size, box_size):
+    grad_x = np.gradient(potential, axis=0)
+    grad_y = np.gradient(potential, axis=1)
+    grad_z = np.gradient(potential, axis=2)
+    if interpolation_method == 'NGP': # Interpolate force from gradients at particle grid locations
+        indices = (positions / box_size * grid_size).astype(int) % grid_size
+        ix, iy, iz = indices[:, 0], indices[:, 1], indices[:, 2]
+        forces = np.stack([grad_x[ix, iy, iz], grad_y[ix, iy, iz], grad_z[ix, iy, iz]], axis=1)
+    elif interpolation_method == 'CIC':
+        force_grid = np.stack([grad_x, grad_y, grad_z], axis=-1)  # shape (Nx,Ny,Nz,3)
+        N_particles = positions.shape[0]
+        cell_size = box_size / grid_size
+        forces = np.zeros((N_particles, 3))
+        positions = positions % box_size
+
+        scaled_pos = positions / cell_size
+        i = np.floor(scaled_pos).astype(int)
+        d = scaled_pos - i
+
+        for p in range(N_particles):
+            base = i[p]
+            w = d[p]
+            for dx in [0, 1]:
+                wx = (1 - w[0]) if dx == 0 else w[0]
+                x = (base[0] + dx) % grid_size
+                for dy in [0, 1]:
+                    wy = (1 - w[1]) if dy == 0 else w[1]
+                    y = (base[1] + dy) % grid_size
+                    for dz in [0, 1]:
+                        wz = (1 - w[2]) if dz == 0 else w[2]
+                        z = (base[2] + dz) % grid_size
+                        weight = wx * wy * wz
+                        forces[p] += force_grid[x, y, z] * weight
+    else:
+        raise ValueError(f"Unknown interpolation method: {interpolation_method}")
+    return forces
+
+
 def force_NGP(potential, positions, grid_size, box_size):
     grad_x = np.gradient(potential, axis=0)
     grad_y = np.gradient(potential, axis=1)
@@ -90,6 +132,37 @@ def force_CIC(potential, positions, grid_size, box_size):
                     forces[p] += force_grid[x, y, z] * weight
     return forces
 
+#Visualisation
+def matplotlib_vis(trajectory, box_size, output_path="Outputs/pm_nbody_sim.mp4", tail_length=30, fade_min=0.05, fps=20):
+    fig = plt.figure(figsize=(8, 8), dpi=80)
+    ax = fig.add_subplot(111, projection='3d')
+    tracked_traj = np.array([frame[0] for frame in trajectory])
+    with iio.get_writer("Outputs/pm_nbody_sim.mp4", fps=20) as writer:
+        for i, frame in enumerate(trajectory):
+            ax.clear()
+            ax.set_xlim(0, box_size)
+            ax.set_ylim(0, box_size)
+            ax.set_zlim(0, box_size)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_zticks([])
+            ax.scatter(frame[:, 0], frame[:, 1], frame[:, 2], s=1, color='black')
+            ax.scatter(frame[0, 0], frame[0, 1], frame[0, 2], s=10, color='red')
+            # Tail generation
+            start = max(0, i - tail_length + 1)
+            tail = tracked_traj[start:i + 1]
+            n_tail = len(tail)
+            if n_tail > 1:
+                for j in range(n_tail - 1):
+                    alpha = fade_min + (1 - fade_min) * (j + 1) / n_tail
+                    ax.plot(tail[j:j + 2, 0], tail[j:j + 2, 1], tail[j:j + 2, 2], color='red', linewidth=1, alpha=alpha)
+            fig.canvas.draw()
+            image = np.array(fig.canvas.buffer_rgba())[..., :3]  # RGB image
+            writer.append_data(image)
+        writer.close()
+
+def pyvista_vis():
+    pass
 
 #Diagnostic Functions
 def compute_kinetic_energy(velocities, masses):
@@ -105,3 +178,6 @@ def compute_potential_energy(positions, masses, potential, grid_size, box_size):
     phi_vals = potential[indices[:,0], indices[:,1], indices[:,2]]
     PE = np.sum(masses * phi_vals) * 0.5  # Factor 0.5 to avoid double-counting
     return PE
+
+def elapsed_time(start_time):
+    return time.time() - start_time
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D  # Needed for 3D plotting
-import imageio as iio
-from simulation_config import *
 from functions import *
+
+#Track Time
+start_time = time.time()
 
 # Initialize particle positions and (zero) velocities
 centre = box_size / 2
@@ -19,16 +18,14 @@ energies = []
 for step in range(steps):
     if interpolation_method == 'NGP':
         density = NGP(positions, grid_size, box_size, masses)
-        potential = compute_potential(density, grid_size)
-        forces = force_NGP(potential, positions, grid_size, box_size)
     elif interpolation_method == 'CIC':
         density = CIC(positions, grid_size, box_size, masses)
-        potential = compute_potential(density, grid_size)
-        forces = force_CIC(potential, positions, grid_size, box_size)
     else:
         raise ValueError(f"Unknown interpolation method: {interpolation_method}")
     if step % 100 == 0:
-        log.debug(f"Step {step}: Computing...")
+        log.info(f"Step {step}: Computing...")
+    potential = compute_potential(density, grid_size)
+    forces = force(potential, positions, grid_size, box_size)
     velocities += forces * dt
     positions += velocities * dt
     positions = positions % box_size  # Apply periodic boundary
@@ -37,6 +34,8 @@ for step in range(steps):
         KE = compute_kinetic_energy(velocities, masses)
         PE = compute_potential_energy(positions, masses, potential, grid_size, box_size)
         energies.append([KE, PE, KE + PE])
+
+print(f"Main loop completed in {elapsed_time(start_time):.2f} seconds.. Initialising visualisation...")
 
 #Track Total Energy
 energies = np.array(energies)
@@ -49,40 +48,8 @@ plt.ylabel('Energy')
 plt.legend()
 plt.show()
 
-#np.save("Outputs/trajectory.npy", np.array(trajectory))
-#np.save("Outputs/energies.npy", energies)
-
-# Visualisation: Make GIF or MP4
-fig = plt.figure(figsize=(8,8), dpi=80)
-ax = fig.add_subplot(111, projection='3d')
-
-# Get trajectory of the tracked particle
-tracked_traj = np.array([frame[0] for frame in trajectory])
-tail_length = 30  # or whatever you like
-fade_min = 0.05   # Minimum opacity for the oldest segment
-
-with iio.get_writer("Outputs/pm_nbody_sim.mp4", fps=20) as writer:
-    for i, frame in enumerate(trajectory):
-        ax.clear()
-        ax.set_xlim(0, box_size)
-        ax.set_ylim(0, box_size)
-        ax.set_zlim(0, box_size)
-        ax.set_xticks([])
-        ax.set_yticks([])
-        ax.set_zticks([])
-        ax.scatter(frame[:,0], frame[:,1], frame[:,2], s=1, color='black')
-        ax.scatter(frame[0,0], frame[0,1], frame[0, 2], s=10, color='red')
-        #Tail generation
-        start = max(0, i - tail_length + 1)
-        tail = tracked_traj[start:i + 1]
-        n_tail = len(tail)
-        if n_tail > 1:
-            for j in range(n_tail - 1):
-                alpha = fade_min + (1 - fade_min) * (j + 1) / n_tail
-                ax.plot(tail[j:j + 2, 0], tail[j:j + 2, 1], tail[j:j + 2, 2], color='red', linewidth=1, alpha=alpha)
-        fig.canvas.draw()
-        image = np.array(fig.canvas.buffer_rgba())[..., :3]  # RGB image
-        writer.append_data(image)
-    writer.close()
+#Visualisation
+matplotlib_vis(trajectory, box_size)
 
 print('Simulation and visualization complete!')
+print(f"Simulation completed in {elapsed_time(start_time):.2f} seconds.")
